325f788d08581b3ac89698f33c086b6fe600c7ed
omarRaafat <omarelgendy79@gmail.com>
2023-10-01 06:16:43 +0300

    update home screen to test pipline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
             @endif
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <h1 style="color:white">******** LARA101 WORKER APP ************</h1>
+            <h1 style="color:white">************ LARA101 K8S APP ************</h1>
                 <div class="flex justify-center">
               
                     <svg viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto bg-gray-100 dark:bg-gray-900">
